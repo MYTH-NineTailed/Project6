05893a8f632050c791ab5efdd8860ecb441684c8
MYTH-NineTailed <91480419+MYTH-NineTailed@users.noreply.github.com>
2021-11-19 11:45:08 +0100

    test

--- index.php
+++ index.php
@@ -4,7 +4,56 @@
 </head>
 
 <body>
-    <p>Testing... Testing... Testing...</p>
+  <h1>Classic Price Tag</h1>
+<div class="product">
+  <div class="product-image"></div> 
+    <span class="price">495,-</span>
+    <p>Very Product</p>
+</div>
+
+<div class="product">
+  <div class="product-image"></div> 
+    <span class="price">1.295,-</span>
+    <p>Truly Useless 5"</p>
+</div>
+<div class="product">
+  <div class="product-image"></div> 
+    <span class="price">625,-</span>
+    <p>Such Features</p>
+</div>
+  <div class="product">
+  <div class="product-image"></div> 
+    <span class="price">495,-</span>
+    <p>Doodle BronzeCast</p>
+</div>
+
+<div class="product">
+  <div class="product-image"></div> 
+    <span class="price">1.795,-</span>
+    <p>Truly Useless 5.7"</p>
+</div>
+<div class="product">
+  <div class="product-image"></div> 
+    <span class="price">625,-</span>
+    <p>Das Produkten</p>
+</div>
+  <div class="product">
+  <div class="product-image"></div> 
+    <span class="price">495,-</span>
+    <p>Very Product</p>
+</div>
+
+<div class="product">
+  <div class="product-image"></div> 
+    <span class="price">1.195,-</span>
+    <p>Super Duper</p>
+</div>
+<div class="product">
+  <div class="product-image"></div> 
+    <span class="price">325,-</span>
+    <p>Very Cheap</p>
+</div>
+  
 </body>
 
 </html>
