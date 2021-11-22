9e01bed25aab06c854c8034d7ff1bc9c03811dc8
MYTH-NineTailed <91480419+MYTH-NineTailed@users.noreply.github.com>
2021-11-22 15:05:46 +0100

    changes made

--- config/search.php
+++ config/search.php
@@ -1,29 +1,14 @@
 <?php]
 
-require '../config/dbconnect.php';
+require 'dbconnect.php';
 
 	if(ISSET($_POST['search'])){
-		$keyword = $_POST['keyword'];
+		$keyword = $_POST['Keyword'];
  
 ?>
 
-<div>
-	<h2>Result</h2>
-	<hr style="border-top:2px dotted #ccc;"/>
-	<?php
-		require 'conn.php';
-		$query = mysqli_query($conn, "SELECT * FROM `blog` WHERE `title` LIKE '%$keyword%' ORDER BY `title`") or die(mysqli_error());
-		while($fetch = mysqli_fetch_array($query)){
-	?>
-	<div style="word-wrap:break-word;">
-		<a href="get_blog.php?id=<?php echo $fetch['blog_id']?>"><h4><?php echo $fetch['title']?></h4></a>
-		<p><?php echo substr($fetch['content'], 0, 100)?>...</p>
-	</div>
-	<hr style="border-bottom:1px solid #ccc;"/>
-	<?php
-		}
-	?>
-</div>
-<?php
-	}
-?>
+
+<html>
+
+
+</html>
