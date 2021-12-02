35f1828480db72ebf22ee961550ca3b0995a6844
cvreugdenhil <9011320@student.zadkine.nl>
2021-12-02 11:36:34 +0100

    gefixte product

--- classes/Product.php
+++ classes/Product.php
@@ -11,7 +11,6 @@ class Product {
     private $price;
     private $description;
     private $code;
-
     private $db;
 
     /**
@@ -22,58 +21,14 @@ class Product {
         $dsn = "mysql:host=localhost;dbname=project_6;charset=utf8mb4";
         $this->db = new PDO($dsn, 'root', '');
     }
-
-    /**
-     * het ophalen van de data
-     */
-    public function getProductsTable()
-    {
-        $stmt = $this->db->query("SELECT product_id FROM product");
- 
-        $stl = "font-weight: bold; background-color: navy; color: #eee; text-transform: uppercase;";
-
-        // bouw een html-table op
-        // met een kopregel (hier opzettelijk td gebruikt in plaats van th)
-        // maar dat is een ontwerp-keuze
-
-        $s = "<table border='1' cellspacing='0' cellpadding='15'>";
-        $s .= "<tr style='$stl'>
-            <td width='2%'>ID</td>
-            <td width='40%'>TITLE</td>
-            <td width='13%'>CATEGORY</td>
-            <td width='10%' align='right'>PRICE</td>
-            <td width='*'>IMAGE</td>
-            </tr>";
-        
-        while($row = $stmt->fetch(PDO::FETCH_OBJ)) {
-
-            $this->getProduct($row->product_id); // haal de properties op van een product!
-
-            // vul een tabelrow voor elk product
-            $s .= "<tr valign='top'>
-                <td>{$this->getId()}</td>
-                <td>{$this->getTitle()}</td>
-                <td>{$this->getCategory()}</td>
-                <td align='right'>{$this->getPrice()}</td>
-                <td>{$this->getImage(250)}</td>
-            </tr>";
-        }
-
-        // en sluit de table af
-        $s .= "</table>";
-
-        return $s; // geef terug aan het aanroeppunt
-    }
-
     
     /**
      * haal de gegevens op van één product
      * en vul de afzonderlijke private properties
      */
-    public function getProduct($pid) {
-        $stmt = $this->db->prepare("SELECT * FROM product 
-                                    WHERE product_id = :pid LIMIT 1");
-        $stmt->execute( ["pid" => $pid] );
+    public function getProduct() {
+        $stmt = $this->db->prepare("SELECT * FROM product");
+        $stmt->execute();
         list($this->id, 
             $this->title, 
             $this->category, 
@@ -85,10 +40,6 @@ class Product {
     /**
      * het ophalen van de beschrijving
      */
-    public function getDescription()
-    {
-        return $this->description;
-    }
 
     public function getId()
     {
@@ -110,41 +61,17 @@ class Product {
         return $this->category;
     }
 
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
     /**
      * het ophalen van de afbeelding. Standaardbreedte is 200, maar kan gezet worden
      */
-    public function getImage($wdt=200)
+    public function getImage($wdt=300)
     {
         return '<img width='."$wdt".' src="data:image/jpeg;base64,'
             . base64_encode($this->image_url).'">';
     }
-
 }
-
-// testen
-$xyz = new Product();
-
-// laat alle producten zien in een overzicht
-echo $xyz->getProductsTable();
-
-echo "<br><hr width='50%'><hr width='70%'><hr width='50%'>";
-
-echo "<h3>ANDERE MOGELIJKHEID:</h3>";
-//    vul de variabelen voor een bepaald product
-//    daarna heb je "toegang" tot de getters
-//    getId(), getTitle(), getCategory(), getImage(), getPrice()
-//
-$xyz->getProduct(4);
-echo "<h1>{$xyz->getId()}: De naam van dit product is ".$xyz->getTitle() . "</h1>"
-    . "Actieprijs is " . $xyz->getPrice();
-
-// even spelen met de foto...
-
-echo "<br>".$xyz->getImage(12); // piepklein fotootje
-echo "<br>".$xyz->getImage(24); // heel klein fotootje
-echo "<br>".$xyz->getImage(48); // kleine foto
-echo "<br>".$xyz->getImage(148); // foto
-echo "<br>".$xyz->getImage(600); // grote foto
-
-
-
--- index.php
+++ index.php
@@ -61,62 +61,16 @@ require 'classes/Product.php';
 </ul>
 </ul>
 
-<!------------- - ------------->
 
 
-<?php $product = new Product(); ?>
-
+<?php $product = new Product();
+$product->getproduct() ?>
 <!-- Producten -->
-  <h1><?php echo intval($title); ?></h1>
 <div class="product">
-  <div class="product-image"><?php echo $product->$image ?></div> 
-    <span class="price"><?php echo $product->$price; ?></span>
-    <p><?php echo $product->$description; ?></p>
+  <div class="product-image"><?php echo $product->getImage();?></div> 
+    <span class="price"><?php echo $product->getPrice(); ?></span>
+    <p><?php echo $product->getTitle(); ?></p>
 </div>
 
-<div class="product">
-  <div class="product-image"></div> 
-    <span class="price">1.295,-</span>
-    <p>Truly Useless 5"</p>
-</div>
-<div class="product">
-  <div class="product-image"></div> 
-    <span class="price">625,-</span>
-    <p>Such Features</p>
-</div>
-  <div class="product">
-  <div class="product-image"></div> 
-    <span class="price">495,-</span>
-    <p>Doodle BronzeCast</p>
-</div>
-
-<div class="product">
-  <div class="product-image"></div> 
-    <span class="price">1.795,-</span>
-    <p>Truly Useless 5.7"</p>
-</div>
-<div class="product">
-  <div class="product-image"></div> 
-    <span class="price">625,-</span>
-    <p>Das Produkten</p>
-</div>
-  <div class="product">
-  <div class="product-image"></div> 
-    <span class="price">495,-</span>
-    <p>Very Product</p>
-</div>
-
-<div class="product">
-  <div class="product-image"></div> 
-    <span class="price">1.195,-</span>
-    <p>Super Duper</p>
-</div>
-<div class="product">
-  <div class="product-image"></div> 
-    <span class="price">325,-</span>
-    <p>Very Cheap</p>
-</div>
-  
 </body>
-
 </html>
