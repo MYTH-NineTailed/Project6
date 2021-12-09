2e91ddb2e452e6580c28ce0924db5e8119d04b7e
cvreugdenhil <9011320@student.zadkine.nl>
2021-12-09 11:36:21 +0100

    eindelijk kunnen inloggen

--- classes/Class.User.php
+++ classes/Class.User.php
@@ -17,7 +17,6 @@ class User {
         $passHash = password_hash($pass, PASSWORD_DEFAULT);
         
         $stmt = $this->db->prepare("INSERT INTO users (userName, userEmail, userPasswordHash) VALUES (?,?,?)");
-        //$stmt->bind_param("sss", $uname, $email, $passHash);
         
         if($stmt->execute( [ $uname, $email, $passHash ] )) {
             return true;
@@ -30,34 +29,24 @@ class User {
     //kijk naar bestaande gebruiker in de database
     public function login($user, $pass) {
         
+
         //Query database for info based on username or email
         $stmt = $this->db->prepare("SELECT userName, userEmail, userPasswordHash FROM users WHERE userName = ? OR userEmail = ?");
-        //$stmt->bind_param("ss", $user, $user);
+       
         $stmt->execute( [ $user, $pass ] );
-        //$stmt->store_result(); // ???
          
-        //als informatie bestaat haal het op, anders een error
-        if ($stmt->num_rows == 1) {            
-            //$stmt->bind_result($uname, $email, $passHash);
-            //$result = $stmt->fetch();
-            
-            list($uname, $email, $uph) = $stmt->fetch(PDO::FETCH_NUM);
+        //als informatie bestaat haal het op, anders een error          
+            list($uname, $email, $passHash) = $stmt->fetch(PDO::FETCH_NUM);
 
             //als wachtwoord overeenkomt start session, anders error
             if (password_verify($pass, $passHash)) {
                 $_SESSION['userName'] = $uname;
                 $_SESSION['userEmail'] = $email;
                 $_SESSION['userLoginStatus'] = 1;
-                
                 return true;
             } else {
                 return 'verkeerd wachtwoord';
             }
-            
-        } else {
-            return 'verkeerd gebruikersnaam of email';
-        }
-        
     }
     
     //Check of de gebruiker is ingelogd
